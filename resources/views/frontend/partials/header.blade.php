76a38a8f1dedcc7477af70280b89504d5c915403
Nabeel Yousaf Pasha <nabeelyousafpasha@gmail.com>
2021-10-31 11:37:17 +0500

    Auth login / registration done

--- resources/views/frontend/partials/header.blade.php
+++ resources/views/frontend/partials/header.blade.php
@@ -1,7 +1,22 @@
 <header class="section page-header">
     <!--RD Navbar-->
     <div class="rd-navbar-wrap">
-        <nav class="rd-navbar rd-navbar-classic" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-md-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-static" data-lg-device-layout="rd-navbar-static" data-xl-layout="rd-navbar-static" data-xl-device-layout="rd-navbar-static" data-lg-stick-up-offset="46px" data-xl-stick-up-offset="46px" data-xxl-stick-up-offset="46px" data-lg-stick-up="true" data-xl-stick-up="true" data-xxl-stick-up="true">
+        <nav class="rd-navbar rd-navbar-classic"
+             data-layout="rd-navbar-fixed"
+             data-sm-layout="rd-navbar-fixed"
+             data-md-layout="rd-navbar-fixed"
+             data-md-device-layout="rd-navbar-fixed"
+             data-lg-layout="rd-navbar-static"
+             data-lg-device-layout="rd-navbar-static"
+             data-xl-layout="rd-navbar-static"
+             data-xl-device-layout="rd-navbar-static"
+             data-lg-stick-up-offset="46px"
+             data-xl-stick-up-offset="46px"
+             data-xxl-stick-up-offset="46px"
+             data-lg-stick-up="true"
+             data-xl-stick-up="true"
+             data-xxl-stick-up="true"
+        >
             <div class="rd-navbar-collapse-toggle rd-navbar-fixed-element-1" data-rd-navbar-toggle=".rd-navbar-collapse"><span></span></div>
             <div class="rd-navbar-aside-outer rd-navbar-collapse bg-gray-900">
                 <div class="rd-navbar-aside">
@@ -10,14 +25,65 @@
                             <li>
                                 <div class="unit unit-spacing-sm align-items-center">
                                     <div class="unit-left"><span class="icon icon-md text-middle fa-phone text-primary"></span></div>
-                                    <div class="unit-body header-phone-wrap">Freephone:&nbsp;<a class="header-phone" href="tel:#">800-2345-6789</a></div>
+                                    <div class="unit-body header-phone-wrap">Phone:&nbsp;<a class="header-phone" href="tel:#">(+92) 321 5031089</a></div>
                                 </div>
                             </li>
                             <li class="header-hours">Opening Hours: 8am-8pm PST M-Th; 6am-3pm PST Fri</li>
                         </ul>
                     </div>
                     <div class="rd-navbar-aside-right">
-                        <div class="group-sm"><a class="button button-sm button-secondary" href="#">Sign In</a><a class="button button-sm button-primary-2" href="#">Register</a></div>
+                        <div class="group-sm">
+                            @if (Route::has('login'))
+                                @auth
+                                    <a href="{{ url('/dashboard') }}" class="button button-sm button-primary-2">Dashboard</a>
+                                    <!-- Settings Dropdown -->
+                                    <div class="hidden sm:flex sm:items-center sm:ml-6">
+                                        <x-dropdown align="right" width="48">
+                                            <x-slot name="trigger">
+                                                <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                                                    <div>{{ Auth::user()->name }}</div>
+
+                                                    <div class="ml-1">
+                                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                                        </svg>
+                                                    </div>
+                                                </button>
+                                            </x-slot>
+
+                                            <x-slot name="content">
+                                                <!-- Authentication -->
+                                                <form method="POST" action="{{ route('logout') }}">
+                                                    @csrf
+
+                                                    <x-dropdown-link :href="route('logout')"
+                                                                     onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                                        {{ __('Log Out') }}
+                                                    </x-dropdown-link>
+                                                </form>
+                                            </x-slot>
+                                        </x-dropdown>
+                                    </div>
+                                @else
+                                    <a
+                                        href="{{ route('login') }}"
+                                        class="button button-sm button-secondary"
+                                    >
+                                        Sign In
+                                    </a>
+
+                                    @if (Route::has('register'))
+                                        <a
+                                            href="{{ route('register') }}"
+                                            class="button button-sm button-primary-2"
+                                        >
+                                            Register
+                                        </a>
+                                    @endif
+                                @endauth
+                            @endif
+                        </div>
                     </div>
                 </div>
             </div>
