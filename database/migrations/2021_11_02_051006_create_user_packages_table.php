aead9e9d6f94b5173bbbf1872d1ac4a9eca7e7bf
Nabeel Yousaf Pasha <nabeelyousafpasha@gmail.com>
2021-11-02 10:15:04 +0500

    model -a for UserPackage

---
+++ database/migrations/2021_11_02_051006_create_user_packages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserPackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_packages', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('package_id')->constrained('packages');
+            $table->unsignedInteger('awarded_bids');
+            $table->decimal('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_packages');
+    }
+}
