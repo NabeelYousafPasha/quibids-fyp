371e369aa4bac3c8cb7d9ede9fdf014f0add4106
Nabeel Yousaf Pasha <nabeelyousafpasha@gmail.com>
2021-11-05 21:06:28 +0500

    removed  from vendor and user

--- resources/views/frontend/pages/vendor/_table.blade.php
+++ resources/views/frontend/pages/vendor/_table.blade.php
@@ -2,7 +2,7 @@
     <thead class="bg-gray-900">
         <tr class="text-white text-left">
             <th class="font-semibold text-sm uppercase px-6 py-4"> Name </th>
-            <th class="font-semibold text-sm uppercase px-6 py-4"> User Name </th>
+            <th class="font-semibold text-sm uppercase px-6 py-4"> Username </th>
             <th class="font-semibold text-sm uppercase px-6 py-4"> Email </th>
             <th class="font-semibold text-sm uppercase px-6 py-4 text-center"> Actions </th>
         </tr>
@@ -25,7 +25,7 @@
                     </p>
                 </td>
                 <td class="px-6 py-4 text-left">
-                    {{ $unApprovedVendor->email }} $
+                    {{ $unApprovedVendor->email }}
                 </td>
                 <td class="px-6 py-4 text-center">
                     <a href="#" class="p-2 pl-5 pr-5 m-1 bg-transparent border-2 border-blue-500 text-blue-500 text-lg rounded-lg hover:bg-blue-500 hover:text-gray-100 focus:border-4 focus:border-blue-300">
