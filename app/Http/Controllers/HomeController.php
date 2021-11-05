2c7de2d4993b9d52b2872e8a0fcdf663f54c1a06
Nabeel Yousaf Pasha <nabeelyousafpasha@gmail.com>
2021-11-06 00:09:00 +0500

    bids reward done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,9 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Package;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $packages = Package::all();
@@ -18,6 +25,10 @@ class HomeController extends Controller
 
     public function dashboard()
     {
-        return view('dashboard');
+        $user = Auth::user();
+
+        return view('dashboard')->with([
+            'authUser' => $user,
+        ]);
     }
 }
